f6dd4682faa7fddf5ac388c63a0571e4f44fef2e
Marcos Oliveira <41500739+terroo@users.noreply.github.com>
2024-07-31 22:51:53 -0300

    Add files via upload

---
+++ main.ter.cpp
@@ -0,0 +1,27 @@
+// A scripting language
+// for C++ programmers
+#include "file.ter";
+
+class std {
+  auto lang;
+  string(lang){
+    this->lang = lang
+    return this->lang;
+  }
+}
+
+set sum(x, y){
+  return x + y;
+}
+
+auto num = sum(36, 42);
+auto obj = std::str("Ter");
+output(var)
+
+output("My Language is: " + obj);
+
+// Faster than Sonic
+for (auto i = 0; i < 1000000; ++i){
+  out("Ok" + i + "\n");
+}
+
